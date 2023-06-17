5962465855e4a6fd677d009419131c09194ce892
tasnimchy96 <tasnim.achy96@gmail.com>
2023-06-18 02:41:29 +0600

    Add add-remove task function

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
-import './style.css';
-import toDoList from './modules/taskList.js';
+import { renderTaskList } from "./modules/taskList.js";
+import { addTask } from "./modules/addTask.js";
+import { initializeListUI } from "./modules/listUI.js";
+import { saveTasks, loadTasks } from "./modules/localStorage.js";
+import { clearCompletedTasks } from "./modules/clearCompletedTasks.js";
 
-const listContainer = document.querySelector('.tasks-container');
+import "./style.css";
 
-const displayTasks = () => {
-  let singleTask = '';
-  toDoList.forEach((task) => {
-    singleTask += `<li class="task-item" id='${task.index}'>
-                    <div class="checkList"> <input type="checkbox" name="check"> ${task.description}</div>
-                    <i class="fa-solid fa-trash"></i>
-                </li>`;
-    listContainer.innerHTML = singleTask;
+let tasks = loadTasks();
+
+const taskInput = document.getElementById("task-input");
+taskInput.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    const description = taskInput.value.trim();
+    if (description !== "") {
+      addTask(tasks, description);
+      taskInput.value = "";
+      saveTasks(tasks);
+    }
+  }
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+  renderTaskList(tasks);
+  initializeListUI();
+
+  const clearButton = document.getElementById("clear-button");
+  clearButton.addEventListener("click", () => {
+    tasks = tasks.filter((task) => !task.completed);
+
+    clearCompletedTasks(tasks);
+    renderTaskList(tasks);
+    saveTasks(tasks);
   });
-};
-
-window.onload = displayTasks();
+});
